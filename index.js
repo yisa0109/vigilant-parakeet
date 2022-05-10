f9b64e350a537159e7d452c49e683a5f7db9a746
yisa0109 <82646780+yisa0109@users.noreply.github.com>
2022-05-10 14:13:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 //User location
-$.getJSON('http://ip-api.com/json', function(data){ //ip-api.com/현재 접속하고 있는 네트워크의 위치를 알려준다.
+$.getJSON('http://ip-api.com/json', 
+function(data){ 
+ 
 var lat = data.lat; //# lat=위도
 var lon = data.lon; //#lon=경도
 var units = "metric"; //#측정단위=미터법
 
 $("#city").html(data.city + ", " + data.country) 
 console.log(data);
-
 //Open weather API request 
 $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=' + units + 
 '&APPID=fb51c6b86eeb06ab9f272171b19b4543', function(json){ 
 console.log(json); 
-$("#temperature-celcius").html(json.main.temp + ' C&deg'); // 섭씨온도 표시
-$("#temperature-farenheit").html((json.main.temp * 1,8 + 32) + ' F&deg'); // 화씨온도 표시
+$("#temp").html(json.main.temp); // 섭씨온도 표시
+$("#style").html(json.main.style) // 옷스타일 표시
 $("#humidity").html(json.main.humidity + ' %'); //습도
 $("#overall").html(json.weather[0].main); // 현재 날씨
-        
 
 
 var apiURI ="http://api.openweathermap.org/data/2.5/weather?q=Busan&appid=fb51c6b86eeb06ab9f272171b19b4543"   
@@ -28,6 +28,5 @@ $('#img').attr("src", imgURL); // 아이콘 표시
 
             }
           });
-
-        });
-    })       
+        })
+      })
