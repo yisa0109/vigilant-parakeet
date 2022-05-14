ddd09020c4951fdf9f9c1e9feb375cfeb73fc5b5
yisa0109 <82646780+yisa0109@users.noreply.github.com>
2022-05-14 12:19:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //User location
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=seoul&appid=fb51c6b86eeb06ab9f272171b19b4543", function(data){ 
+$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=seoul&appid=fb51c6b86eeb06ab9f272171b19b4543', function(data){ 
 
 var lat = data.lat; //# lat=위도
 var lon = data.lon; //#lon=경도
