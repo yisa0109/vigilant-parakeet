0c29cbb74e63e4b47fe75a399f08d9bc43cea842
yisa0109 <82646780+yisa0109@users.noreply.github.com>
2022-05-14 14:58:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ var units = "metric"; //#측정단위=미터법
 $("#city").html(data.city + ", " + data.country) 
 console.log(data);
 //Open weather API request 
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=' + units + 
-'&APPID=fb51c6b86eeb06ab9f272171b19b4543', function(json){ 
+$.getJSON('index.json', function(json){ 
 console.log(json); 
 $("#temp").html(json.main.temp); // 섭씨온도 표시
 $("#style").html(json.main.style) // 옷스타일 표시
@@ -17,7 +16,7 @@ $("#humidity").html(json.main.humidity + ' %'); //습도
 $("#overall").html(json.weather[0].main); // 현재 날씨
 	
 
-var apiURI ="http://api.openweathermap.org/data/2.5/weather?q=seoul&appid=fb51c6b86eeb06ab9f272171b19b4543"   
+var apiURI ="index.json" 
           $.ajax({
             url : apiURI,method : 'GET',success :  (data)=> {// 아이콘 취득 
 
