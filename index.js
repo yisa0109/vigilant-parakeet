cd06542642bf610c86cd86effa829ac25349555c
yisa0109 <82646780+yisa0109@users.noreply.github.com>
2022-05-14 12:31:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -15,45 +15,8 @@ $("#temp").html(json.main.temp); // 섭씨온도 표시
 $("#style").html(json.main.style) // 옷스타일 표시
 $("#humidity").html(json.main.humidity + ' %'); //습도
 $("#overall").html(json.weather[0].main); // 현재 날씨
-
 	
-var style = prompt("지금 날씨에 적당한 옷 차림은?"); ///각 온도에 따라 글자는 넣었으나 이미지는 못넣었습니다.
-				
-{
-  if(style <= 4){
-    document.images[0].src =('4~.png')
-    document.images[0].width = 600;
-    document.images[0].height = 200;
-  }else if(style >= 5 && style < 8){
-    document.images[0].src =('5~8.png')
-    document.images[0].width = 600;
-    document.images[0].height = 200;					
-  }else if(style >= 9 && style < 12){
-    document.images[0].src =('9~11.png')
-    document.images[0].width = 600;
-    document.images[0].height = 200;
-  }else if(style >= 12 && style < 17){
-    document.images[0].src =('12~16.png')
-    document.images[0].width = 600;
-    document.images[0].height = 200;
-  }else if(style >= 17 && style < 20){
-    document.images[0].src =('17~19.png')
-    document.images[0].width = 600;
-    document.images[0].height = 200;
-  }else if(style >= 20 && style < 23){
-    document.images[0].src =('20~22.png')
-    document.images[0].width = 600;
-    document.images[0].height = 200;
-  }else if(style >= 23 && style < 28){
-    document.images[0].src =('23~27.png')
-    document.images[0].width = 600;
-    document.images[0].height = 200;
-  }else if(style >= 28){
-    document.images[0].src =('28.png')
-    document.images[0].width = 600;
-    document.images[0].height = 200;
-  }
-}
+
 
 var apiURI ="http://api.openweathermap.org/data/2.5/weather?q=seoul&appid=fb51c6b86eeb06ab9f272171b19b4543"   
           $.ajax({
