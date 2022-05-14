6b7981a47172a7db1b6a2444976d4e19c34c1d23
yisa0109 <82646780+yisa0109@users.noreply.github.com>
2022-05-15 01:37:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //User location
-$.getJSON('https://api.openweathermap.org/data/2.5/weather?lat=35.1028&lon=129.0403&APPID=1844db1080e02fd7f0b9e3b8136ec4bf', function(data){ 
+$.getJSON('https://api.openweathermap.org/data/2.5/weather?id=1838524&appid=1844db1080e02fd7f0b9e3b8136ec4bf', function(data){ 
 
 var lat = data.lat; //# lat=위도
 var lon = data.lon; //#lon=경도
@@ -8,7 +8,7 @@ var units = "metric"; //#측정단위=미터법
 $("#name").html(data.name) 
 
 //Open weather API request 
-$.getJSON('https://api.openweathermap.org/data/2.5/weather?lat=35.1028&lon=129.0403&APPID=1844db1080e02fd7f0b9e3b8136ec4bf', function(json){ 
+$.getJSON('https://api.openweathermap.org/data/2.5/weather?id=1838524&appid=1844db1080e02fd7f0b9e3b8136ec4bf', function(json){ 
 
 $("#temp").html((json.main.temp+(-273.15))); // 섭씨온도 표시
 $("#style").html(json.main.style) // 옷스타일 표시
@@ -18,7 +18,7 @@ $("#overall").html(json.weather[0].main); // 현재 날씨
 
 	
 	
-var apiURI ="https://api.openweathermap.org/data/2.5/weather?lat=35.1028&lon=129.0403&APPID=1844db1080e02fd7f0b9e3b8136ec4bf"
+var apiURI ="https://api.openweathermap.org/data/2.5/weather?id=1838524&appid=1844db1080e02fd7f0b9e3b8136ec4bf"
           $.ajax({
             url : apiURI,method : 'GET',success :  (data)=> {// 아이콘 취득 
 
