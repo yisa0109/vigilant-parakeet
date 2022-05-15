9e12aa425f784034a8484ed19342e742d54cd063
yisa0109 <82646780+yisa0109@users.noreply.github.com>
2022-05-15 11:45:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ $("#name").html(data.name)
 //Open weather API request 
 $.getJSON('https://api.openweathermap.org/data/2.5/weather?id=1838524&appid=1844db1080e02fd7f0b9e3b8136ec4bf&units=metric', function(json){ 
 
-$("#temp").html((json.main.temp ' C&deg');// 섭씨온도 표시
+$("#temp").html(json.main.temp ' C&deg');// 섭씨온도 표시
 $("#style").html(json.main.style) // 옷스타일 표시
 $("#humidity").html(json.main.humidity + ' %'); //습도
 $("#overall").html(json.weather[0].main); // 현재 날씨
