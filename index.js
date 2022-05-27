1d64e02d584931c5b414fbf1448cbe1d1f5b4ff7
yisa0109 <82646780+yisa0109@users.noreply.github.com>
2022-05-28 04:34:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ var apiURI ="https://api.openweathermap.org/data/2.5/weather?lat=35.1028&lon=129
 $('#img').attr("src", imgURL); // 아이콘 표시
 		    
 $("#style").html(json.main.style) // 옷스타일 표시
-		    var style = document.getElementById(("temp").value)
+		    var style = document.getElementById(("temp").src)
+		    let temp=style
 						if(style <= 4){
 							document.images[0].src =('4~.png')
 							document.images[0].width = 600;
