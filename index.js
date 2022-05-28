0309d731fa85000c6c48d83b4290ee19b406dfaf
yisa0109 <82646780+yisa0109@users.noreply.github.com>
2022-05-29 02:13:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //User location
-$.getJSON('https://ipinfo.io/', function(data){ 
+$.getJSON('https://api.openweathermap.org/data/2.5/weather?lat=35.1028&lon=129.0403&appid=1844db1080e02fd7f0b9e3b8136ec4bf&units=metric', function(data){ 
 $("#name").html(data.name) 
 //Open weather API request 
-$.getJSON('https://ipinfo.io/', function(json){ 
+$.getJSON('https://api.openweathermap.org/data/2.5/weather?lat=35.1028&lon=129.0403&appid=1844db1080e02fd7f0b9e3b8136ec4bf&units=metric', function(json){ 
 $("#temp").html(json.main.temp + ' C&deg')// 섭씨온도 표시
 $("#humidity").html(json.main.humidity + ' %'); //습도
 $("#overall").html(json.weather[0].main); // 현재 날씨
@@ -13,7 +13,6 @@ var apiURI ="https://api.openweathermap.org/data/2.5/weather?lat=35.1028&lon=129
               var imgURL = "//openweathermap.org/img/w/" + data.weather[0].icon + ".png"; 
 $('#img').attr("src", imgURL); // 아이콘 표시
 		    
-
 
 		    
 $("#style").html(json.main.style) // 옷스타일 표시
